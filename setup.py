19c93eb333ed567255824b673ba5f2eabd5f73ba
masseyagus <agustingabriel2002@gmail.com>
2025-05-10 03:04:16 -0300

    Inicialización de setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "Trabajo Integrador Final 2025",
+    version = "0.0.0",
+    author = "Agustín Quintana",
+    author_email = "agustingabriel2002@gmail.com",
+    description = "...",
+    python_requires = ">3.9"
+)
