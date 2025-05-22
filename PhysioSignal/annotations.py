3eaa62fe4a4620901da10e5bd21b64610397f3e7
Agustín Quintana <agustingabriel2002@gmail.com>
2025-05-22 16:06:29 -0300

    Clase Annotations, futura integración

---
+++ PhysioSignal/annotations.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+@dataclass
+class Annotations:
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get_annotations(self):
+        pass
+
+    def find(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
+    pass
