d3a7da67eda6deba0ec7d680699a9266fa835649
Agustín Quintana <agustingabriel2002@gmail.com>
2025-06-13 17:58:33 -0300

    Inicio get_data()

--- physiosignal/signals/raw.py
+++ physiosignal/signals/raw.py
@@ -7,6 +7,7 @@ from physiosignal.info import Info
 from physiosignal.info import Annotations
 from physiosignal.logger import log_config
 import matplotlib.pyplot as plt
+import numpy as np
 import logging
 
 # Configuración global del logger
@@ -17,11 +18,30 @@ logger = logging.getLogger(__name__)
 # Futura implementación
 class RawSignal:
 
-    def __init__(self, data, sfrq, info:Info, anotaciones:Annotations, first_samp):
-        pass
+    def __init__(self, data:np.ndarray=None, sfreq:float=None, info:Info=None, anotaciones:Annotations=None, first_samp:int=0):
 
-    def get_data(self, picks, start, stop, reject, times):
-        pass
+        self.data = data # Matriz con forma (n_canales, n_muestras)
+        self.info = info # Objeto Info
+        self.antoaciones = anotaciones # Objeto Annotations
+        self.sfreq = self.info.sfreq if sfreq is None else sfreq
+        self.first_samp = first_samp # Índice de la primera muestra
+
+
+    def get_data(self, picks:str|np.array=None, start:float=0, stop:float=0, reject:float=0, times:bool=False):
+        
+        begin, end = int(start * self.sfreq), int(stop * self.sfreq)
+
+        if start and stop:
+            return self.data[:, begin:end]
+        elif start:
+            return self.data[:, begin:]
+        elif stop:
+            return self.data[:, :end]
+        else:
+            sec = int(10 * self.sfreq)
+            return self.data[:, :sec]
+            
+            
 
     def drop_channels(self, ch_names) -> RawSignal:
         pass
