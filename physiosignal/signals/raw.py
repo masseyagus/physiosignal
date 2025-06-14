4b85fb72600504fec0394ee95a474133365847a8
Agustín Quintana <agustingabriel2002@gmail.com>
2025-06-14 04:22:52 -0300

    Correcciones y mejoras get_data()

--- physiosignal/signals/raw.py
+++ physiosignal/signals/raw.py
@@ -29,19 +29,53 @@ class RawSignal:
 
     def get_data(self, picks:str|np.array=None, start:float=0, stop:float=0, reject:float=0, times:bool=False):
         
+        duration = self.data.shape[1]/self.sfreq
+
+        if start < 0 or stop < 0 or start > stop or stop > duration:
+            raise ValueError(f"Valores inválidos: start debe estar entre [0, {round(duration, 1)}], " 
+                             f"y stop no debe exceder {round(duration, 1)}")
+        
         begin, end = int(start * self.sfreq), int(stop * self.sfreq)
 
         if start and stop:
-            return self.data[:, begin:end]
+            data = self.data[:, begin:end]
         elif start:
-            return self.data[:, begin:]
+            data = self.data[:, begin:]
         elif stop:
-            return self.data[:, :end]
+            data = self.data[:, :end]
         else:
-            sec = int(10 * self.sfreq)
-            return self.data[:, :sec]
-            
-            
+            sec = int(10 * self.sfreq) # Para 0, muestro 10 seg
+            data = self.data[:, :sec]
+
+        if picks is None:
+            return data
+
+        if isinstance(picks, str): # Canal en formato str
+            try:
+                idx = self.info.ch_names.index(picks)
+            except:
+                raise ValueError(f"El canal {picks} no existe dentro de ch_names")
+            return data[idx, :]
+
+        elif isinstance(picks, (list, tuple)): # varios canales
+            idx = []
+            for ch in picks:
+                if isinstance(ch, str):
+                    try:
+                        idx.append(self.info.ch_names.index(ch))    
+                    except:
+                        raise ValueError(f"El canal {ch} no se encuentra dentro de ch_names")
+
+                elif isinstance(ch, int):
+                    idx.append(ch)
+
+            return data[idx,:]     
+        
+        elif isinstance(picks, int):
+            return data[picks, :]
+        
+        else:
+            raise TypeError(f"El parámetro 'picks' debe ser str, int, list o tuple")
 
     def drop_channels(self, ch_names) -> RawSignal:
         pass
