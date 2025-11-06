4ddc662c64dc6fadcb68602e8570e3481c120275
Agustín Quintana <agustingabriel2002@gmail.com>
2025-11-06 19:58:06 -0300

    Actualización de versión

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="physiosignal",
-    version="1.6.0",
+    version="1.7.0",
     description="Librería para procesamiento de señales EEG, ECG y EMG",
     author="Agustín Quintana",
     packages=find_packages(),
